165b89973c3d93f6aa89e2240b4d482542c40391
mlmstdt <johan.malmstedt@gmail.com>
2023-10-10 18:01:10 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+import * as THREE from 'https://unpkg.com/three@0.134.0/build/three.module.js';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create a cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Handle window resize
+window.addEventListener('resize', () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    
+    camera.aspect = newWidth / newHeight;
+    camera.updateProjectionMatrix();
+    
+    renderer.setSize(newWidth, newHeight);
+});
+
+// Animation function
+const animate = () => {
+    requestAnimationFrame(animate);
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
+    renderer.render(scene, camera);
+};
+
+// Start the animation
+animate();
